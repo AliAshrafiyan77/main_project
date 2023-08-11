cd60255fe4f1e8962c04a618cef86564530a33ae
Ali Ashrafiyan <aliashrafiyanms@gmail.com>
2023-08-11 11:10:44 +0330

    =fixed bug

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,20 @@
 import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    resolve: {
-        mainFields: [
-            'browser',
-            'module',
-            'main',
-            'jsnext:main',
-            'jsnext'
-        ],
-        alias: {
-            'vue': 'vue/dist/vue.esm-bundler',
-        }
-    },
     plugins: [
-        vue(),
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/scss/auth.scss'
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
